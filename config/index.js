fc35c0a284f8b982c07b5fe5d78066d1bdfbdcac
mayongfei <919700733@qq.com>
2018-05-12 18:52:22 +0800

    Update port

--- config/index.js
+++ config/index.js
@@ -14,7 +14,7 @@ module.exports = {
 
     // Various Dev Server settings
     host: '172.20.110.125', // can be overwritten by process.env.HOST
-    port: 4000, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
